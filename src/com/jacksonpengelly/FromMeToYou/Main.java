4f585b2d8bac9c014fa30e70af069f072fe4d4f3
j.pengelly <jfpngly@gmail.com>
2026-08-30 15:28:16 -0400

    From Me To You
    + Completed From Me To You, which is just a simple println program.

---
+++ src/com/jacksonpengelly/FromMeToYou/Main.java
@@ -0,0 +1,16 @@
+package com.jacksonpengelly.FromMeToYou;
+
+/*
+Author: Jackson Pengelly
+Date created: Aug 30, 2026
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("From: Bill Smith");
+        System.out.println("Address: Dell Computer, Bldg 13");
+        System.out.println("Date: April 12, 2005");
+        System.out.println("\nTo: Jack Jones");
+        System.out.println("\nMessage: Help! I'm trapped inside a computer!");
+    }
+}
