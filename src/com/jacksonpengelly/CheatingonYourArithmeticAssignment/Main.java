32fd7937b5ca6c646c8a889a7ccddbb0eef82023
j.pengelly <jfpngly@gmail.com>
2026-08-30 15:38:55 -0400

    Cheating On Your Arithmetic Assignment
    + Completed Cheating On Your Arithmetic Assignment, which involves basic arithmetic.

---
+++ src/com/jacksonpengelly/CheatingonYourArithmeticAssignment/Main.java
@@ -0,0 +1,9 @@
+package com.jacksonpengelly.CheatingonYourArithmeticAssignment;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + (79 + 3 * (4 + 82 - 68) - 7 + 19));
+        System.out.println("(179 + 21 + 10) / 7 + 181 = " + ((179 + 21 + 10) / 7 + 181));
+        System.out.println("10389 * 56 * 11 + 2246 = " + (10389 * 56 * 11 + 2246));
+    }
+}
